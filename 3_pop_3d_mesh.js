914ec02e9d577f556fcc80a7011821466c0a75ad
miraimeisatu <43312017+miraimeisatu@users.noreply.github.com>
2021-10-02 21:29:13 +0900

    Add files via upload

---
+++ 3_pop_3d_mesh.js
@@ -0,0 +1,87 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmFsdWVjcmVhdGlvbiIsImEiOiJja2swcm9pdWMwazk2MnB0ZzZ5NTVwMXAxIn0.9klPq_GB5cZ0lRZVdZR2_g';
+
+const {DeckGL, GeoJsonLayer} = deck;
+const COLOR_SCALE = [
+  [44, 123, 182], //  36000 - 40000
+  [100, 165, 205], // 32000 - 36000
+  [157, 207, 228], // 28000 - 32000
+  [199, 230, 219], // 24000 - 28000
+  [237, 247, 201], // 20000 - 24000
+  [255, 237, 170], // 16000 - 20000
+  [254, 201, 128], // 12000 - 16000
+  [254, 201, 128], // 12000 - 16000
+  [249, 158, 89], // 8000 - 12000
+  [232, 91, 58], // 4000 - 8000
+  [215, 25, 28] // 0 - 4000
+];
+
+const geojsonLayer = new GeoJsonLayer({
+    data: 'https://raw.githubusercontent.com/valuecreation/mapbox-prj/b014b62e2c4db92726ca35ca8ec9a52b2acd5f28/data/1km_mesh_2018_13.geojson',
+    opacity: 0.3,
+    stroked: false,
+    filled: true,
+    extruded: true,
+    wireframe: true,
+    fp64: true,
+    getElevation: f => f.properties.PT0_2050,
+    getFillColor: f => colorScale(f.properties.PT0_2050),
+    getLineColor: [255, 255, 255],
+    pickable: true,
+    onHover: updateTooltip
+});
+
+new DeckGL({
+    mapboxApiAccessToken: mapboxgl.accessToken,
+    mapStyle: 'mapbox://styles/mapbox/dark-v9',
+    latitude: 35.681236,
+    longitude: 139.767125,
+    zoom: 9,
+    maxZoom: 16,
+    pitch: 45,
+    layers: [geojsonLayer]
+});
+
+function colorScale(x) {
+
+  if (0 >= x && x < 4000) {
+    return COLOR_SCALE[0];
+  } else if (4000 >= x && x < 8000) {
+    return COLOR_SCALE[1];
+  } else if (8000 >= x && x < 8000) {
+    return COLOR_SCALE[2];
+  } else if (12000 >= x && x < 16000) {
+    return COLOR_SCALE[3];
+  } else if (16000 >= x && x < 20000) {
+    return COLOR_SCALE[4];
+  } else if (20000 >= x && x < 24000) {
+    return COLOR_SCALE[5];
+  } else if (24000 >= x && x < 28000) {
+    return COLOR_SCALE[6];
+  } else if (28000 >= x && x < 32000) {
+    return COLOR_SCALE[7];
+  } else if (32000 >= x && x < 36000) {
+    return COLOR_SCALE[8];
+  } else if (36000 >= x && x < 40000) {
+    return COLOR_SCALE[9];
+  } else if (40000 > x) {
+    return COLOR_SCALE[9];
+  }
+}
+
+function updateTooltip({x, y, object}) {
+  const tooltip = document.getElementById('tooltip');
+  if (object) {
+    tooltip.style.top = `${y}px`;
+    tooltip.style.left = `${x}px`;
+    tooltip.innerHTML = `
+      <div><b>City code &nbsp;</b></div>
+      <div><div>${object.properties.SHICODE}</div></div>
+      <div><b>1/1/2022 Patient Risk</b></div>
+      <div>${Math.round(object.properties.PT0_2050)}人</div>
+      `;
+  } else { 
+    tooltip.innerHTML = '';
+  }
+}
+
